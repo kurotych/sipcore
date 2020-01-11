4d12c8d6d75c37c1905be3b028a900590e8b1d90
Anatolii Kurotych <akurotych@gmail.com>
2020-01-11 11:41:07 +0200

    Add parse request method

--- crates/sipmsg/src/request.rs
+++ crates/sipmsg/src/request.rs
@@ -4,29 +4,38 @@ use nom::{
     sequence::tuple,
 };
 
+use crate::header::*;
+use alloc::vec::Vec;
+
 use core::{str, u8};
 
 pub struct Request<'a> {
     // The request line
-    pub rl: Option<RequestLine<'a>>,
-    //The request headers.
-    // pub headers: &'a [Header<'a>],
+    pub rl: RequestLine<'a>,
+    /// The request headers.
+    pub headers: Vec<Header<'a>>,
+    /// Body
+    pub body: Option<&'a [u8]>,
 }
 
 impl<'a> Request<'a> {
-    // fn new() -> Request<'a> {
-    //     Request {
-    //         // headers: []{Header{na}},
-    //         rl : None,
-    //     }
-    // }
+    fn new(rl: RequestLine<'a>, headers: Vec<Header<'a>>, body: Option<&'a [u8]>) -> Request<'a> {
+        Request {
+            rl: rl,
+            headers: headers,
+            body: body,
+        }
+    }
 
-    // pub fn parse(buf_input: &'buf [u8]) -> nom::IResult<&[u8], Request> {
-    //     let new_request = Request::new();
-    //     new_request.request_buffer = Some(buf_input);
-    //     let (_, rl) = RequestLine::parse(buf_input)?;
-    //     Ok((buf_input, new_request))
-    // }
+    pub fn parse(buf_input: &'a [u8]) -> nom::IResult<&[u8], Request> {
+        let (input, rl) = RequestLine::parse(buf_input)?;
+
+        let (input, headers) = parse_headers(input)?;
+        // TODO check header Content-Length
+        let (body, _) = tag("\r\n")(input)?;
+
+        Ok((buf_input, Request::new(rl, headers, Some(body))))
+    }
 }
 
 /// SIP-Version
@@ -148,7 +157,9 @@ impl Method {
 
 #[cfg(test)]
 mod tests {
+    use crate::request;
     use crate::request::Method;
+    use crate::request::Request;
     use crate::request::RequestLine;
     use crate::request::SipVersion;
 
@@ -165,6 +176,64 @@ mod tests {
                 assert_eq!(rl.uri, expected_uri);
             }
             Err(_e) => panic!(),
+        }
+    }
+
+    fn check_header_value(
+        result_header: &request::Header,
+        exp_h_name: &str,
+        exp_h_value: &str,
+        exp_h_parameters: Option<&str>,
+    ) {
+        assert_eq!(result_header.name, exp_h_name);
+        assert_eq!(result_header.value, exp_h_value);
+        assert_eq!(result_header.parameters, exp_h_parameters);
+    }
+
+    #[test]
+    fn parse_request() {
+        let invite_msg_buf = "INVITE sip:bob@biloxi.com SIP/2.0\r\n\
+                              Via: SIP/2.0/UDP pc33.atlanta.com;branch=z9hG4bKkjshdyff\r\n\
+                              To: Bob <sip:bob@biloxi.com>\r\n\
+                              From: Alice <sip:alice@atlanta.com>;tag=88sja8x\r\n\
+                              Max-Forwards: 70\r\n\
+                              Call-ID: 987asjd97y7atg\r\n\
+                              CSeq: 986759 INVITE\r\n\r\nbody_stuff"
+            .as_bytes();
+
+        match Request::parse(invite_msg_buf) {
+            Ok((_, parsed_req)) => {
+                assert_eq!(parsed_req.rl.method, Method::INVITE);
+                assert_eq!(parsed_req.rl.sip_version, SipVersion(2, 0));
+                assert_eq!(parsed_req.rl.uri, "sip:bob@biloxi.com");
+
+                assert_eq!(parsed_req.headers.len(), 6);
+
+                check_header_value(
+                    &parsed_req.headers[0],
+                    "Via",
+                    "SIP/2.0/UDP pc33.atlanta.com",
+                    Some("branch=z9hG4bKkjshdyff"),
+                );
+                check_header_value(
+                    &parsed_req.headers[1],
+                    "To",
+                    "Bob <sip:bob@biloxi.com>",
+                    None,
+                );
+                check_header_value(
+                    &parsed_req.headers[2],
+                    "From",
+                    "Alice <sip:alice@atlanta.com>",
+                    Some("tag=88sja8x"),
+                );
+                check_header_value(&parsed_req.headers[3], "Max-Forwards", "70", None);
+                check_header_value(&parsed_req.headers[4], "Call-ID", "987asjd97y7atg", None);
+                check_header_value(&parsed_req.headers[5], "CSeq", "986759 INVITE", None);
+
+                assert_eq!(parsed_req.body.unwrap(), "body_stuff".as_bytes())
+            }
+            Err(_) => panic!(),
         }
     }
 
