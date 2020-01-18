1c8ffe6e614131857c80407c4fb264c27ada196f
Anatolii Kurotych <akurotych@gmail.com>
2020-01-18 12:37:45 +0200

    Add parameters.rs

--- crates/sipmsg/src/lib.rs
+++ crates/sipmsg/src/lib.rs
@@ -1,20 +1,21 @@
 #![no_std]
 
-extern crate nom;
 extern crate alloc;
+extern crate nom;
 
 pub mod header;
-pub use header::Header;
 pub use header::parse_headers;
+pub use header::Header;
+mod parameters;
 
 pub mod message;
-pub use message::MessageType;
 pub use message::get_message_type;
+pub use message::MessageType;
 pub use message::SipVersion;
 
 pub mod request;
-pub use request::RequestLine;
 pub use request::Request;
+pub use request::RequestLine;
 
 pub mod response;
 pub use response::Response;
