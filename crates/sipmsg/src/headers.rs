99c3ae439794f1f27eae8bdb1c3504e6b0bb99b7
Anatolii Kurotych <akurotych@gmail.com>
2020-02-25 21:09:35 +0200

    Add headers.rs

---
+++ crates/sipmsg/src/headers.rs
@@ -0,0 +1,77 @@
+/// Contained of SIP message headers
+
+use crate::errorparse::SipParseError;
+use crate::traits::NomParser;
+use crate::header::Header;
+use alloc::collections::VecDeque;
+use alloc::collections::btree_map::BTreeMap;
+use unicase::Ascii;
+
+const CRLF: &[u8] = &[0x0d, 0x0a]; // /r/n
+
+pub struct Headers<'a> {
+    headers :  BTreeMap<Ascii<&'a str>, VecDeque<Header<'a>>>
+}
+
+impl<'a> Headers<'a> {
+    pub fn get(&self, key: &'a str) -> Option<&VecDeque<Header<'a>>> {
+        self.headers.get(&Ascii::new(key))
+    }
+}
+
+impl<'a> NomParser<'a> for Headers<'a> {
+    type ParseResult = Headers<'a>;
+
+    fn parse(input: &'a [u8]) -> nom::IResult<&[u8], Self::ParseResult, SipParseError>
+    {
+        let mut headers = BTreeMap::<Ascii<&'a str>, VecDeque<Header<'a>>>::new();
+        let mut inp2 = input;
+        loop {
+            match Header::parse(inp2) {
+                Ok((inp, header)) => {
+                    if headers.contains_key(&header.name) {
+                        headers.get_mut(&header.name).unwrap().push_front(header)
+                    } else {
+                        let mut vec: VecDeque<Header<'a>> = VecDeque::new();
+                        vec.push_front(header);
+                        headers.insert(vec[0].name, vec);
+                    }
+                    inp2 = inp;
+                }
+                Err(e) => return Err(e),
+            }
+            if inp2.len() > 1 && &inp2[0..2] == CRLF {
+                // end of headers and start of body part
+                break;
+            }
+        }
+        Ok((inp2, Headers{ headers: headers}))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn headers_parse_test(){
+        let parse_headers_result = Headers::parse(
+            "To: sip:user@example.com\r\n\
+             Route: <sip:192.0.2.254:5060>\r\n\
+             Route: <sip:[2001:db8::1]>\r\n\
+             Max-Forwards: 70\r\n\
+             Call-ID: lwsdisp.1234abcd@funky.example.com\r\n\
+             CSeq: 60 OPTIONS\r\n\
+             Via: SIP/2.0/UDP funky.example.com;branch=z9hG4bKkdjuw\r\n\r\nsomebody"
+                .as_bytes(),
+        );
+
+        match parse_headers_result {
+            Ok((_, hdrs)) => {
+                assert_eq!(hdrs.get("Route").unwrap()[1].value, "<sip:192.0.2.254:5060>");
+                assert_eq!(hdrs.get("rouTe").unwrap()[0].value, "<sip:[2001:db8::1]>");
+                assert_eq!(hdrs.get("route").unwrap().len(), 2);
+            },
+            Err(_) => panic!()
+        }
+    }
+}
