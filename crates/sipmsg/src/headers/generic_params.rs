2add0951f79094348b6d2584d63fabad15ae4eef
Anatolii Kurotych <akurotych@gmail.com>
2020-11-28 17:26:47 +0200

    Fix GenericParams parser. Add supporting quoted string and IPV6

--- crates/sipmsg/src/headers/generic_params.rs
+++ crates/sipmsg/src/headers/generic_params.rs
@@ -1,18 +1,21 @@
 use crate::common::{
     bnfcore::is_token_char,
     errorparse::SipParseError,
-    nom_wrappers::{from_utf8_nom, take_while_trim_sws},
+    hostport::HostPort,
+    nom_wrappers::{from_utf8_nom, take_quoted_string, take_sws, take_while_trim_sws},
+    take_sws_token,
 };
 use alloc::collections::btree_map::BTreeMap;
-use nom::multi::many0;
+use nom::{bytes::complete::take_while, multi::many0};
 use unicase::Ascii;
 
-// generic-param  =  token [ EQUAL gen-value ]
 pub struct GenericParam<'a> {
-    /// Sip header name
+    /// Parameter name
     pub name: Ascii<&'a str>,
-    /// Sip header value
+    /// Param value, without quotes and "[" if it ipv6
     pub value: Option<&'a str>,
+    // TODO add raw representation param
+    // It needs at least to dump quoted params as is
 }
 
 impl<'a> GenericParam<'a> {
@@ -22,18 +25,25 @@ impl<'a> GenericParam<'a> {
         let (input, (_, parameter_name, _)) = take_while_trim_sws(input, is_token_char)?;
 
         let (_, param_name) = from_utf8_nom(parameter_name)?;
-
         if input.is_empty() || input[0] != b'=' {
             return Ok((input, (Ascii::new(param_name), None)));
         }
+        let (input, _) = take_sws_token::equal(input)?;
 
-        if input.len() == 1 {
+        if input.is_empty() {
             return sip_parse_error!(2, "generic-param parse error");
         }
 
-        let (input, (_, parameter_value, _)) =
-            take_while_trim_sws(&input[1..] /* skip '=' */, is_token_char)?;
+        let (input, parameter_value) = if input[0] == b'"' {
+            let (input, (_, param_val, _)) = take_quoted_string(input).unwrap();
+            (input, param_val)
+        } else if input[0] == b'[' {
+            HostPort::take_ipv6_host(input)?
+        } else {
+            take_while(is_token_char)(input)?
+        };
 
+        let (input, _) = take_sws(input)?;
         let (_, parameter_value) = from_utf8_nom(parameter_value)?;
 
         Ok((input, (Ascii::new(param_name), Some(parameter_value))))
@@ -84,50 +94,36 @@ mod tests {
     }
     #[test]
     fn patameters_contains_test() {
-        match GenericParams::parse(";a=b\r\n".as_bytes()) {
-            Ok((inp, gparams)) => {
-                assert_eq!(gparams.contains("a"), true);
-                assert_eq!(gparams.contains("b"), false);
-                assert_eq!(gparams.contains("c"), false);
-                assert_eq!(inp.len(), 2)
-            }
-            Err(_) => panic!(),
-        }
+        let (inp, params) = GenericParams::parse(";a=b\r\n".as_bytes()).unwrap();
+        assert_eq!(params.contains("a"), true);
+        assert_eq!(params.contains("b"), false);
+        assert_eq!(params.contains("c"), false);
+        assert_eq!(inp.len(), 2)
     }
 
     #[test]
     fn patameters_correct_parse_test() {
-        match GenericParams::parse(";a=b\r\n".as_bytes()) {
-            Ok((inp, gparams)) => {
-                assert_eq_gp(&gparams, "a", Some("b"));
-                assert_eq!(inp.len(), 2)
-            }
-            Err(_) => panic!(),
-        }
+        let (inp, params) = GenericParams::parse(";a=b\r\n".as_bytes()).unwrap();
+        assert_eq_gp(&params, "a", Some("b"));
+        assert_eq!(inp.len(), 2);
 
-        match GenericParams::parse(";a;n=q;c\r\n".as_bytes()) {
-            Ok((inp, gparams)) => {
-                assert_eq_gp(&gparams, "a", None);
-                assert_eq_gp(&gparams, "n", Some("q"));
-                assert_eq_gp(&gparams, "c", None);
-                assert_eq!(gparams.get("qq"), None);
-                assert_eq!(inp.len(), 2)
-            }
-            Err(_) => panic!(),
-        }
+        let (inp, params) = GenericParams::parse(";a;n=q;c\r\n".as_bytes()).unwrap();
+        assert_eq_gp(&params, "a", None);
+        assert_eq_gp(&params, "n", Some("q"));
+        assert_eq_gp(&params, "c", None);
+        assert_eq!(params.get("qq"), None);
+        assert_eq!(inp.len(), 2);
 
-        match GenericParams::parse("; aw ;d =es;sam;mark= a; wam = kram; q = 0.3\r\n".as_bytes()) {
-            Ok((i, gparams)) => {
-                assert_eq_gp(&gparams, "aw", None);
-                assert_eq_gp(&gparams, "d", Some("es"));
-                assert_eq_gp(&gparams, "sam", None);
-                assert_eq_gp(&gparams, "mark", Some("a"));
-                assert_eq_gp(&gparams, "wam", Some("kram"));
-                assert_eq_gp(&gparams, "q", Some("0.3"));
-                assert_eq!(i.len(), 2)
-            }
-            Err(_) => panic!(),
-        }
+        let (inp, params) =
+            GenericParams::parse("; aw ;d =es;sam;mark= a; wam = kram; q = 0.3\r\n".as_bytes())
+                .unwrap();
+        assert_eq_gp(&params, "aw", None);
+        assert_eq_gp(&params, "d", Some("es"));
+        assert_eq_gp(&params, "sam", None);
+        assert_eq_gp(&params, "mark", Some("a"));
+        assert_eq_gp(&params, "wam", Some("kram"));
+        assert_eq_gp(&params, "q", Some("0.3"));
+        assert_eq!(inp.len(), 2);
     }
 
     fn parameter_test(
@@ -136,8 +132,7 @@ mod tests {
         expected_value: Option<&str>,
         expected_len: usize,
     ) {
-        let res = GenericParam::parse(input_str.as_bytes());
-        let (i, (name, value)) = res.unwrap();
+        let (i, (name, value)) = GenericParam::parse(input_str.as_bytes()).unwrap();
         assert_eq!(name, expected_name);
         assert_eq!(value, expected_value);
         assert_eq!(i.len(), expected_len);
@@ -160,16 +155,31 @@ mod tests {
         parameter_test("a\r\n", "a", None, 2);
         parameter_test("a=b\r\n", "a", Some("b"), 2);
         parameter_test("a=b;123", "a", Some("b"), 4);
-        parameter_test(" a  \r\n", "a", None, 2);
-        parameter_test(" aw = b \r\n", "aw", Some("b"), 2);
+        parameter_test(" a\r\n", "a", None, 2);
+        parameter_test(" aw = b\r\n", "aw", Some("b"), 2);
         parameter_test(" aw = b; \r\n", "aw", Some("b"), 4);
         parameter_test("q=0.001", "q", Some("0.001"), 0);
+        parameter_test(
+            "+sip.instance=\"<urn:uuid:00000000-0000-1000-8000-000A95A0E128>\"",
+            "+sip.instance",
+            Some("<urn:uuid:00000000-0000-1000-8000-000A95A0E128>"),
+            0,
+        );
+        parameter_test(
+            "received=[2001:db8::9:255]",
+            "received",
+            Some("2001:db8::9:255"),
+            0,
+        );
+        // According to rfc we should take until it is not token char
+        // It seems validation of next chars it is not deal of this class.
+        // But i'm not sure
+        parameter_test("a=bla$bla", "a", Some("bla"), 4);
     }
 
     #[test]
     fn parameter_incorrect_parse_test() {
         fail_parameter_test("");
         fail_parameter_test("a=");
-        fail_parameter_test("a=/");
     }
 }
